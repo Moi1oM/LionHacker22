606e9f076aae49ea852599b1cbc1aebc8a254e5f
Moi1ioM <hcloud0806@gmail.com>
2022-08-08 12:38:03 +0900

    2022.08.08

---
+++ vacationProject/json map/seven_eleven_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="seven_eleven_tileset" tilewidth="16" tileheight="16" tilecount="54" columns="6">
+ <image source="../../../seven_eleven_gbstudio_tileset/seven_eleven_tileset.png" width="104" height="152"/>
+</tileset>
